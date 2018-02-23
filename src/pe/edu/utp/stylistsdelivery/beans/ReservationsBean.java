373a4b92690053f4f4242e3615c3fa37c38d049c
Administrador <1310591@utp.edu.pe>
2018-02-22 19:33:14 -0500

    comit

--- src/pe/edu/utp/stylistsdelivery/beans/ReservationsBean.java
+++ src/pe/edu/utp/stylistsdelivery/beans/ReservationsBean.java
@@ -1,4 +1,6 @@
 package pe.edu.utp.stylistsdelivery.beans;
 
+
+
 public class ReservationsBean {
 }
--- src/pe/edu/utp/stylistsdelivery/models/User.java
+++ src/pe/edu/utp/stylistsdelivery/models/User.java
@@ -1,4 +1,29 @@
 package pe.edu.utp.stylistsdelivery.models;
 
-public class User {
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@SessionScoped
+@Named (value = "login")
+public class User implements Serializable{
+    private String userNamed;
+    private String password;
+
+    public String getUserNamed() {
+        return userNamed;
+    }
+
+    public void setUserNamed(String userNamed) {
+        this.userNamed = userNamed;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
