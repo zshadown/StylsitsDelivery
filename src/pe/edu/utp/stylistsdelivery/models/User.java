7add5d4a6a92061bdc23e2929069af4dd02bc783
Administrador <1310591@utp.edu.pe>
2018-02-22 19:43:43 -0500

    comit

--- src/pe/edu/utp/stylistsdelivery/models/User.java
+++ src/pe/edu/utp/stylistsdelivery/models/User.java
@@ -1,29 +1,4 @@
 package pe.edu.utp.stylistsdelivery.models;
 
-
-import javax.enterprise.context.SessionScoped;
-import javax.inject.Named;
-import java.io.Serializable;
-
-@SessionScoped
-@Named (value = "login")
-public class User implements Serializable{
-    private String userNamed;
-    private String password;
-
-    public String getUserNamed() {
-        return userNamed;
-    }
-
-    public void setUserNamed(String userNamed) {
-        this.userNamed = userNamed;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+public class User {
 }
