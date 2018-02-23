6820effcbc823fe77badefb4c11238f3ad1f8e2d
Edwin <1322528@utp.edu.pe>
2018-02-22 19:48:11 -0500

    Configurando2...4

--- src/pe/edu/utp/stylistsdelivery/models/Service.java
+++ src/pe/edu/utp/stylistsdelivery/models/Service.java
@@ -1,4 +1,9 @@
 package pe.edu.utp.stylistsdelivery.models;
 
 public class Service {
+    private int id;
+    private String description;
+    private double cost;
+    private int time;
+    private User user;
 }
