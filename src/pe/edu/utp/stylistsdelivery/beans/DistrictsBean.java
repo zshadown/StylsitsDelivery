d0f8dcd62deb311e914ac1fff0cbba3d4e0e28ed
Administrador <1310591@utp.edu.pe>
2018-02-24 17:25:53 -0500

    desarroalndo service y districtsbean

--- src/pe/edu/utp/stylistsdelivery/beans/DistrictsBean.java
+++ src/pe/edu/utp/stylistsdelivery/beans/DistrictsBean.java
@@ -1,13 +1,45 @@
 package pe.edu.utp.stylistsdelivery.beans;
 
+import pe.edu.utp.stylistsdelivery.models.Province;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
 //Districts Beans
-public class DistrictsBean {
+public class DistrictsBean implements Serializable {
+    private int id;
+    private String description;
+    private Province province;
 
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
+    public Province getProvince() {
+        return province;
+    }
 
+    public void setProvince(Province province) {
+        this.province = province;
+    }
+    public String dovalidateInput(){
+        if (!"sample".equals(id) ) return "error";
+    }
 
 }
